0010edaed844c70315d3f4ce2aae59a88d8af15f
Raunak1303 <161805979+Raunak1303@users.noreply.github.com>
2025-06-18 09:53:47 +0530

    Update CORS error

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://real-estate-price-predictor.vercel.app"
+        "https://real-estate-price-predictor-six.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
